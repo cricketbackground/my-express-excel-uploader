089b09f193be8df926cdf3c5958312b38a24daeb
cricketbackground <vadzz.java@gmail.com>
2021-02-15 22:57:22 -0500

    added excel reader

--- routes/upload.js
+++ routes/upload.js
@@ -2,12 +2,25 @@ var express = require('express');
 var router = express.Router();
 var multer  = require('multer')
 var upload = multer({ dest: './public/data/uploads/' })
+const readXlsxFile = require('read-excel-file/node');
+var fs = require('fs');
 
 // The uploading will be completed once the middleware returns, i.e. once this route is hit.
 router.post('/', upload.single('orders_request'), function (req, res) {
     // req.file is the name of your file in the form above, here 'orders_request'
     // req.body will hold the text fields, if there were any
     console.log(req.file, req.body);
+
+    // Read excel file as a readable stream.
+    // https://www.npmjs.com/package/read-excel-file
+    readXlsxFile(fs.createReadStream(req.file.path)).then((rows) => {
+        // `rows` is an array of rows
+        console.log(rows);
+    }).catch(reason => {
+        console.error("Reading XSLX File Failed", reason)
+    });
+
+    //
     return res.status(200).json({
         message: 'file submitted successfully',
         fieldName: req.file.fieldname,
@@ -18,6 +31,7 @@ router.post('/', upload.single('orders_request'), function (req, res) {
         path: req.file.path,
         size: req.file.size
     });
+
 });
 
 module.exports = router;
