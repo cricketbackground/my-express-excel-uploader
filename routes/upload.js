49e56c95b2f4af4b6404532808bf454d80c2dd04
cricketbackground <vadzz.java@gmail.com>
2021-02-15 22:34:35 -0500

    added comments

--- routes/upload.js
+++ routes/upload.js
@@ -3,8 +3,9 @@ var router = express.Router();
 var multer  = require('multer')
 var upload = multer({ dest: './public/data/uploads/' })
 
+// The uploading will be completed once the middleware returns, i.e. once this route is hit.
 router.post('/', upload.single('orders_request'), function (req, res) {
-    // req.file is the name of your file in the form above, here 'uploaded_file'
+    // req.file is the name of your file in the form above, here 'orders_request'
     // req.body will hold the text fields, if there were any
     console.log(req.file, req.body);
     return res.status(200).json({
